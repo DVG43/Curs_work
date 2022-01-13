b3417c20fc2676ac9e209104f843e1874b34ddfd
Galuta Dmitrii <dvgaluta@gmail.com>
2022-01-14 01:00:50 +0300

    third_day

--- curs_prog_galuta.py
+++ curs_prog_galuta.py
@@ -2,21 +2,28 @@
 # with open('token.txt','r') as file_object:
 #      token = file_object.read().strip()
 
-# impotr time
+#impotr time
 import requests
 from pprint import pprint
 
 # URL = 'https://api.vk.com/method/users.get'
 
 # Пишем класс для инициализации.
-class Vk_user:
-     URL = 'https://api.vk.com/method/'
-     def __init__(self, token, version):
-          self.params = {
-               'access_token' : token,
-               'ver' : version
-          }
+# class Vk_user:
+#      URL = 'https://api.vk.com/method/'
+#      def __init__(self, token, version):
+#           self.params = {
+#                'access_token' : token,
+#                'v' : version
+#           }
 
+class VkUser:
+    url = 'https://api.vk.com/method/'
+    def __init__(self, token, version):
+        self.params = {
+                 'access_token': token,
+                 'v': version
+        }
 # Пишем методы для класса.
      # Параметры sort:
      # 0 - по умолчанию
@@ -26,17 +33,17 @@ class Vk_user:
      # 4 - по отношению количества комментариев к количеству пользоват
      # 5 - по отношениею количества записей в обсуждениях к количеству пользователей
 
-     def search_groups (self, q, sorting=0):
-          group_search_url = self.URL + 'group.search'
-          group_search_params = {
-               'q':  q,
-               'sort':  sorting,
-               'count' : 300
+    def search_groups(self, q, sorting=0):
+        group_search_url = self.url + 'groups.search'
+        group_search_params = {
+              'q': q,
+              'sort': sorting,
+              'count': 300
           }
-          req = requests.get(group_search_url, params={**self.params, **group_search_params}).json()
-          return req ['response']['items']
+        req = requests.get(group_search_url, params={**self.params, **group_search_params}).json()
+        return req['response']['items']
 
 if __name__ == '__main__':
      token = '958eb5d439726565e9333aa30e50e0f937ee432e927f0dbd541c541887d919a7c56f95c04217915c32008'
-     vk_i_user = Vk_user(token, '5.131')
-     pprint (vk_i_user.search_groups('python'))
+     vk_i_user = VkUser(token, '5.131')
+     pprint (vk_i_user.search_groups('Михаил Булгаков'))
--- my_poligon.py
+++ my_poligon.py
@@ -1,12 +1,32 @@
 import requests
 from pprint import pprint
 
-URL = 'https://api.vk.com/method/users.get'
+# URL = 'https://api.vk.com/method/users.get'
 token = '958eb5d439726565e9333aa30e50e0f937ee432e927f0dbd541c541887d919a7c56f95c04217915c32008'
+URL = 'https://api.vk.com/method/account.getProfileInfo'
 params = {
-    'user_id' : '1000',
+    'account':'begemot_korovin',
     'access_token' : token,
     'v' : '5.131'
 }
 res = requests.get(URL,params=params)
 pprint(res.json())
+
+#token = '958eb5d439726565e9333aa30e50e0f937ee432e927f0dbd541c541887d919a7c56f95c04217915c32008'
+URL = 'https://api.vk.com/method/photos.get'
+params = {
+    'owner_id' : '552934290',
+    'album_id' : 'profile',
+    'extended' : 1,
+    'photo_sizes' : 0,
+    'access_token' : token,
+    'v' : '5.131'
+}
+res = requests.get(URL,params=params)
+pprint(res.json())
+
+
+
+# account.getProfileInfo
+# begemot_korovin
+
